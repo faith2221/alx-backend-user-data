9742265ada1bd1c50474d2b0cb8aecb5d1248196
faith2221 <edithbanda014@gmail.com>
2024-05-30 16:10:48 +0000

    correction

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,18 +6,18 @@ Encrypting passwords.
 import bcrypt
 
 
-def hash_password(pasword: str) -> bytes:
+def hash_password(password: str) -> bytes:
     """
     A function that expects one string argument name
     password and return a salted, hashed password, which is a byte string.
     """
-    if password:
-        return bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     A function that expect 2 arguments and returns a boolean.
     """
-    if hashed_password and password:
-        return bcrypt.checkpw(str.encode(password), hashed_password)
+    return bcrypt.checkpw(password.encode(), hashed_password)
